7c8359469f475e9b9a7c7a138e65d1209b525f2d
Milenati Federico <fedvm@hotmail.com>
2022-11-01 21:44:38 -0300

    primera seccion del home y footer

---
+++ src/components/Categorias/categoria/Categoria.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import { Container, Row, Col } from 'reactstrap';
+
+import categoriaImg1 from "../../../assets/Imagenes/futbol.png";
+import categoriaImg2 from "../../../assets/Imagenes/basquet.png";
+import categoriaImg3 from "../../../assets/Imagenes/runing.png";
+
+import "../../../styles/categoria.css";
+
+const datosCategoria = [
+    {
+        display:"Futbol",
+        imgUrl: categoriaImg1
+    },
+    {
+        display:"Basquet",
+        imgUrl: categoriaImg2
+    },
+    {
+        display:"Running",
+        imgUrl: categoriaImg3
+    },
+]
+
+const Categoria = () => {
+    return <Container>
+        <Row>
+
+            {datosCategoria.map((item, index) => (
+                <Col lg="4" md="4"  >
+                    <div className='item_categoria d-flex justify-content-center gap-3'>
+                        <div className='img_categoria'>
+                            <img src={item.imgUrl} alt="item_categoria" />
+                        </div>
+                        <h6>{item.display}</h6>
+                    </div>
+                </Col>
+            ))}            
+        </Row>
+    </Container>
+};
+
+export default Categoria;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,13 @@ import Helmet from "../components/Helmet/Helmet";
 import { Container, Row, Col } from 'reactstrap';
 
 import imgprinc from "../assets/Imagenes/imagenprincipal.jpg";
-
+import isegur from "../assets/Imagenes/iconoseguridad.png";
 import "../styles/home.css";
+
+
+import {Link} from "react-router-dom";
+
+import Categoria from "../components/Categorias/categoria/Categoria";
 
 const Home = () => {
     return <Helmet title="Home">
@@ -14,7 +19,28 @@ const Home = () => {
                 <Row>
                     <Col lg="6" md="6">
                     <div className='contenido_imagen'>
-
+                        <h5 className='mb-3'>
+                            Importamos desde USA
+                        </h5>
+                            <h1 className='mb-4 titulohome'>
+                                <span>MARCA </span> JORDAN 
+                            </h1>
+                                <p>Si te gustan las AIR JORDAN,<br />contactate con nostoros
+                                </p>
+                                    <div className='botonhome d-flex align-items-center gap-5 mt-4'>
+                                        <button className='registrate d-flex align-items-center justify-content-between'>
+                                            Registrate
+                                        </button>
+                                        <button className='boton_cate'>
+                                            <Link to="/Categorias">
+                                                Categorias
+                                            </Link>
+                                        </button>
+                                    </div>
+                                    <div className='detalle d-flex align-items-center gap-5 mt-3'>
+                                        <p className='d-flex align-items-center gap-2'><span className='icono_registro'><img src={isegur} alt="iconoSeguridad" /></span>
+                                        100 % Seguro</p>
+                                    </div>
                     </div>
                     </Col>
 
@@ -27,6 +53,10 @@ const Home = () => {
                 </Row>
             </Container>
         </section>
+        <section>
+            <Categoria />
+        </section>
+
     </Helmet>
 };
 
